0de5846944b3f69b271b85bd323c1979f842f890
Aidan <ajberger905@gmail.com>
2023-08-19 13:46:40 -0400

    upload Component Complete

--- client/App.js
+++ client/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Upload } from './components/Upload'
 
 const App = () => {
     return(
         <div>
             <h1>Online Art Gallery</h1>
+            <Upload />
         </div>
     )
 }
