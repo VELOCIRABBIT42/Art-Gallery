d65d0054c2b44a3f8c52cc51bca76d40c51ad82f
sharmarke <shar.alternatif@gmail.com>
2023-08-20 09:38:13 -0400

    users image can be uploaded to firebase

--- client/components/Upload.js
+++ client/components/Upload.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
+import { storage } from '../firebase';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { v4 } from 'uuid'
 
 
 export function Upload() {
@@ -11,8 +14,8 @@ const [description, setDescription] = useState('');
 
 
 const getImg = (event) => {
-  setImage(event.target.value);
-  console.log(event.target.value)
+  setImage(event.target.files[0]);
+  console.log('local image',event.target.files[0])
 }
 
 const getTitle = (event) => {
@@ -26,14 +29,40 @@ const getDescription = (event) => {
 }
 
 const upload = async () => {
+  if (image === null) return;
+  const imageRef = ref(storage,`images/${image.name + v4()}`);
+  // uploadBytes(imageRef, image).then((data) => {
+  //   alert('Image uploaded');
+  //   getDownloadURL(data.ref).then((url) => {
+  //     console.log('url', url);
+
+  //   })
+
+  // });
+
+  try{
+    const response = await uploadBytes(imageRef, image)
+    alert('Image uploaded');
+    const url = await getDownloadURL(response.ref);
+    console.log('the image url on firebase', url)
+  }catch (err) {
+    console.log('Error while uploading image to firebase:', err)
+  }
+
+
+
+
+ // Server side functionality not yet implemented
   try {
-    await fetch('localhost:3000/gallery/upload', {
+    await fetch('localhost:3000/upload', {
     method: 'POST',
     body: JSON.stringify({image: image, title: title, description: description})
     })
   } catch (err) {
     console.log(err)
   }
+
+  
 
 }
 
@@ -49,3 +78,7 @@ const upload = async () => {
   )
 
 }
+
+
+
+
