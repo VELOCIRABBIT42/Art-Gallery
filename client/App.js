5184f4d8e891d82bee01e6fee4a8bb5e09696704
Aidan <ajberger905@gmail.com>
2023-08-21 10:11:33 -0400

    front end'

--- client/App.js
+++ client/App.js
@@ -3,17 +3,33 @@ import CardContainer from './components/CardContainer';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Upload } from './components/Upload';
 import { Login } from './components/Login';
-import CardComponent  from './components/CardComponent';
+import CardComponent from './components/CardComponent';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+// import { Signup } from './components/Signup';
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/main' element={<CardContainer />} />
-        <Route path='/upload' element={<Upload />} />
-        <Route path='/component' element={ <CardComponent />} />
-      </Routes>
+      <Navbar />
+      {/* <div className='container'>
+        <div className='row'>
+          <div className="col-2"> */}
+          <div className="d-flex">
+          <Sidebar />
+          {/* </div>
+          <div className="col"> */}
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/main' element={<CardContainer />} />
+            <Route path='/upload' element={<Upload />} />
+            <Route path='/component' element={<CardComponent />} />
+            {/* <Route path='/signup' element={<Signup />} /> */}
+          </Routes>
+          </div>
+          {/* </div>
+        </div>
+      </div> */}
     </>
   );
 };
--- client/components/CardComponent.js
+++ client/components/CardComponent.js
@@ -11,22 +11,51 @@ const CardComponent = ({ image }) => {
     // </div>
     <div className='container'>
       <div className='row'>
-        <div className='col' style={{width:100}}>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/1200px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg' className="img-fluid"/>
+        <div className='col-4'>
+          <div className='card h-100 shadow'>
+            <div className='d-flex flex-column justify-content align-items-center'>
+              <img
+                src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/1200px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
+                className='img-fluid card-img-top'
+              />
+              <div className='card-body'>
+                <h5 className='card-title'>Mona Lisa</h5>
+                <p className='card-text'>Artist name</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="col">
-        <h5>The Mona Lisa</h5>
-        <p>Artist: Leonardo da Vinci</p>
+        <div className='col-4'>
+          <div className='card h-100 shadow'>
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+            <img
+              src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Great_Wave_off_Kanagawa2.jpg/330px-Great_Wave_off_Kanagawa2.jpg'
+              className='img-fluid card-img-top'
+            />
+            <div className='card-body'>
+              <h5 className='card-title'>Mona Lisa</h5>
+              <p className='card-text'>Artist name</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='col'>
-          <div style={{width: 100}}>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Great_Wave_off_Kanagawa2.jpg/330px-Great_Wave_off_Kanagawa2.jpg' className="img-fluid"/>
-        </div>
-        <h5>The Mona Lisa</h5>
-        <p>Artist: Leonardo da Vinci</p>
+        <div className='col-4'>
+          <div className='card h-100 shadow'>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+            <img
+              src='https://img.freepik.com/free-photo/abstract-eye-portrait-young-women-elegance-generated-by-ai_188544-9712.jpg'
+              className='img-fluid card-img-top'
+            />
+            <div className='card-body'>
+              <h5 className='card-title'>Mona Lisa</h5>
+              <p className='card-text'>Artist name</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+
   );
 };
 
